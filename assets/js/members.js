699a5b22a44c6c9c87a6971c2ba1064c7039b374
altinodanas <altinodantas@hotmail.com>
2019-02-22 14:15:43 -0300

    Update the formate of the json file with members

--- assets/js/members.js
+++ assets/js/members.js
@@ -1,36 +1,34 @@
 $("document").ready(function(){
-	
    	
-	$.getJSON("data/members.json", function(result){
+	$.getJSON("data/members.list.json", function(result){
 		
-		var undergraduates = result.undergraduate.filter(activeMember);
+    var undergraduates = getMemberByType("undergraduate", result.members)
 		
-        $.each(undergraduates, function(i, member){			
-				
-			var html = '<div class="col-sm-6"> '+
-                  '<div class="row" id="row-member">'+
-                    '<div class="col-lg-2 col-sm-3 col-xs-4" id="img-member">'+
-                      '<img src="'+member.avatar+'" class="img-thumbnail"/>'+
-                    '</div>'+
-                    '<div class="col-lg-10 col-sm-9 col-xs-8">'+
-                      '<h4>'+ member.name +'</h4>'+
-                      '<p><b>Since: </b> ' + member.date + ' | ' + member.mail +' | <a target="_blank" href="'+member.homepage+'">Webpage</a><br />'+
-                      '<b>Research: </b>'+member.research+'</p>'+
-                    '</div>'+
+    $.each(undergraduates, function(i, member){			
+      
+    var html = '<div class="col-sm-6"> '+
+                '<div class="row" id="row-member">'+
+                  '<div class="col-lg-2 col-sm-3 col-xs-4" id="img-member">'+
+                    '<img src="'+member.avatar+'" class="img-thumbnail"/>'+
                   '</div>'+
-
-                '</div>'
-				
-			// used to clear flot style after the second item in each pseudo-line	
-			if(i % 2 == 1)
-				html += '<div style="clear:both">';
-				
-            $("#all-undergraduate").append(html);
-			      $("#undergraduates").append(html);
-			
-        });
+                  '<div class="col-lg-10 col-sm-9 col-xs-8">'+
+                    '<h4>'+ member.name +'</h4>'+
+                    '<p><b>Since: </b> ' + member.date + ' | ' + member.mail +' | <a target="_blank" href="'+member.homepage+'">Webpage</a><br />'+
+                    '<b>Research: </b>'+member.research+'</p>'+
+                  '</div>'+
+                '</div>'+
+              '</div>'
+      
+    // used to clear flot style after the second item in each pseudo-line	
+    if(i % 2 == 1)
+      html += '<div style="clear:both">';
+      
+          $("#all-undergraduate").append(html);
+          $("#undergraduates").append(html);
+    
+      });
 		
-		var masters = result.master.filter(activeMember);
+		var masters = getMemberByType("master", result.members)
 		
 		$.each(masters, function(i, member){			
 				
@@ -45,7 +43,6 @@ $("document").ready(function(){
                       '<b>Research: </b>'+member.research+'</p>'+
                     '</div>'+
                   '</div>'+
-
                 '</div>'
 			
 			// used to clear flot style after the second item in each pseudo-line	
@@ -57,7 +54,7 @@ $("document").ready(function(){
 					
         });
 		
-		var phds = result.phd.filter(activeMember);
+		var phds = getMemberByType("doctoral", result.members)
 		
 		$.each(phds, function(i, member){			
 				
@@ -72,19 +69,16 @@ $("document").ready(function(){
                       '<b>Research: </b>'+member.research+'</p>'+
                     '</div>'+
                   '</div>'+
-
                 '</div>'
 			
 			// used to clear flot style after the second item in each pseudo-line	
 			if(i % 2 == 1)
 				html += '<div style="clear:both">';	
-			
             $("#all-phd").append(html);
-			$("#phds").append(html);
-					
+			      $("#phds").append(html);
         });
 		
-		var formers = result.former.filter(activeMember);
+		var formers = getMemberByType("former", result.members)
 		
 		$.each(formers, function(i, member){			
 				
@@ -99,19 +93,15 @@ $("document").ready(function(){
                       '<b>Research: </b>'+member.research+'</p>'+
                     '</div>'+
                   '</div>'+
-
                 '</div>'
 				
 			// used to clear flot style after the second item in each pseudo-line	
 			if(i % 2 == 1)
 				html += '<div style="clear:both">';
-				
             $("#all-former").append(html);
-			$("#formers").append(html);
-					
+			      $("#formers").append(html);
         });
-		
-    });
+  });
 	
 });
 
@@ -120,3 +110,14 @@ function activeMember(member){
 		return true;
 	return false;
 }
+
+function getMemberByType(mType, membersVector){
+  // Return all active members of a given type: undergraduate | master | doctoral | former
+  return membersVector.filter(function(member){
+    if (member.active && member.position == mType)
+      return true
+  })
+
+}
+
+
